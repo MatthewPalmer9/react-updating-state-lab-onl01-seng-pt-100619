a15c91fe4db588aa320e58f8f5c9da211002d36f
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2020-06-22 17:01:21 -0500

    Done.

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,40 @@
 // Code YouTubeDebugger Component Here
+import React, {Component} from 'react';
+
+export default class YouTubeDebugger extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    adjustSettings = (adjustment) => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                ...adjustment
+            }
+        })
+    }
+
+    adjustBitrate = () => this.adjustSettings({bitrate: 12})
+    adjustResolution = () => this.adjustSettings({video: {resolution: '720p'}})
+
+    render() {
+        return(
+            <div>
+                <button className={"bitrate"} onClick={this.adjustBitrate}></button>
+                <button className={"resolution"} onClick={this.adjustResolution}></button>
+            </div>
+        )
+    }
+}
